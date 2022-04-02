43ef1a79de01c4adcd5cf893ae74668812e07b6d
Keenan Matrix <keenanmatrixpt@gmail.com>
2022-04-02 19:07:10 -0400

    moved theme.js out of pages directory to stop deployment errors

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import {ChakraProvider} from '@chakra-ui/react'
 import {MoralisProvider} from 'react-moralis'
-import theme from './theme'
+import theme from '../theme'
 function MyApp({Component, pageProps}) {
   return (
     <MoralisProvider
